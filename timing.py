7029b98dcd1acd1f235d664f11880847f2d8f133
Ryan Kim <ryankima@umich.edu>
2024-12-02 23:20:30 +0000

    timing p2

--- timing.py
+++ timing.py
@@ -1,25 +1,55 @@
 import subprocess
 import sys
 import time
+import psutil
+
+NUM_SAMPLES = 10
+
+def monitor_memory(pid):
+    try:
+        process = psutil.Process(pid)
+        peak_memory = 0
+    
+        while process.is_running() and process.status() != psutil.STATUS_ZOMBIE:
+            try:
+                mem_info = process.memory_info()
+                peak_memory = max(peak_memory, mem_info.rss)
+            except psutil.NoSuchProcess:
+                break
+        return peak_memory
+    except psutil.NoSuchProcess:
+        print("prozesse ist schon gestorben")
+        return 0
 
 def measure_execution_time(command):
     try:
+        avg_time = 0
+        avg_mem = 0
+        
+        stdout = ""
+        stderr = ""
         # Start the timer
-        start_time = time.perf_counter()
+        for i in range(NUM_SAMPLES):
+            start_time = time.perf_counter()
         
-        # Run the command
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            # Run the command
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            pid = process.pid
+            peak_memory = monitor_memory(pid)
         
-        # Stop the timer
-        end_time = time.perf_counter()
-        
-        # Calculate elapsed time
-        elapsed_time = end_time - start_time
+            # Stop the timer
+            end_time = time.perf_counter()
+            stdout, stderr = process.communicate() 
+            # Calculate elapsed time
+            elapsed_time = end_time - start_time
+            avg_time = ((avg_time * i) + elapsed_time) / (i + 1)
+            avg_mem = ((avg_mem * i) + peak_memory) / (i + 1)
         
         # Output the program's stdout, stderr, and execution time
-        print("Output:\n", result.stdout)
-        print("Error:\n", result.stderr if result.stderr else "None")
-        print(f"Execution Time: {elapsed_time:.4f} seconds")
+        print("Output:\n", stdout)
+        print("Error:\n", stderr if stderr else "None")
+        print(f"Execution Time: {avg_time:.4f} seconds")
+        print(f"Peak memory: {avg_mem / 1024 / 1024:.2f} MB")
         
     except Exception as e:
         print(f"An error occurred: {e}")
